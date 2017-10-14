3fa2f12df9c6c69ac2c2ba014bcb8809518d6718
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-14 10:02:05 -0500

    first commit

---
+++ LoanCalculator/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  LoanCalculator
+//
+//  Created by Peter on 10/14/17.
+//  Copyright © 2017 Peter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+//    initialize variables
+    float Loan, Rate, Years, Balance, Term, Payment;
+//    end program
+    return 0;
+}
