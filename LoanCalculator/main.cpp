6fc838ec47a9e5d59dd831fed06be30b682c2363
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-15 10:57:02 -0500

    inputs and math

--- LoanCalculator/main.cpp
+++ LoanCalculator/main.cpp
@@ -7,10 +7,21 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <math.h>
 
 int main(int argc, const char * argv[]) {
 //    initialize variables
     float Loan, Rate, Years, Balance, Term, Payment;
+    std::cout << "\t\t Loan Calculator \n\n";
+    std::cout << "Please enter the loan amount: \n";
+    std::cin >> Loan;
+    std::cout << "Please enter the loan rate: \n";
+    std::cin >> Rate;
+    std::cout << "Please enter the length in years of the loan: \n";
+    std::cin >> Years;
+    Term = pow((1 + Rate / 12), 12 * Years);
+    Payment = (Loan * Rate / 12 * Term) / (Term-1);
 //    end program
     return 0;
 }
