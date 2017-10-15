e6b72e61235a96ac82df5a62440cc4e918c75ee8
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-15 11:55:53 -0500

    finished

--- LoanCalculator/main.cpp
+++ LoanCalculator/main.cpp
@@ -12,7 +12,10 @@
 
 int main(int argc, const char * argv[]) {
 //    initialize variables
+    int miy;
     float Loan, Rate, Years, Balance, Term, Payment;
+    miy = 12;
+//    input info
     std::cout << "\t\t Loan Calculator \n\n";
     std::cout << "Please enter the loan amount: \n";
     std::cin >> Loan;
@@ -20,8 +23,33 @@ int main(int argc, const char * argv[]) {
     std::cin >> Rate;
     std::cout << "Please enter the length in years of the loan: \n";
     std::cin >> Years;
-    Term = pow((1 + Rate / 12), 12 * Years);
-    Payment = (Loan * Rate / 12 * Term) / (Term-1);
+//    calculation
+    Term = pow((1 + Rate / miy), miy * Years);
+    Payment = (Loan * Rate / miy * Term) / (Term-1);
+//    display
+//      formatting
+    std::cout.precision(2);
+    std::cout.setf(std::ios::fixed | std::ios::showpoint | std::ios::left);
+    std::cout << "Monthly payment: $" << Payment << std::endl;
+    std::cout << std::endl;
+    std::cout << std::setw(10) << "Month";
+    std::cout << std::setw(10) << "Interest";
+    std::cout << std::setw(10) << "Principal";
+    std::cout << std::setw(10) << "Balance" << std::endl;
+    std::cout << "---------------------------------------\n";
+    Balance = Loan;
+//    loop to display monthly payments
+    for (int Month = 0; Month < (miy * Years); Month++){
+        float monthly, Principal;
+//        monthly interest
+        monthly = Rate / miy * Balance;
+        Principal = Payment - monthly;
+        std::cout << std::setw(10) << (Month + 1);
+        std::cout << std::setw(10) << monthly;
+        std::cout << std::setw(10) << Principal;
+        std::cout << std::setw(10) << Balance << std::endl;
+        Balance -= Principal;
+    }
 //    end program
     return 0;
 }
